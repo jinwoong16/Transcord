556eaac7e3a907d582cab4c65a74322a74c44506
jinwoong <baseball0020@gmail.com>
2023-10-05 21:01:37 +0900

    Initial Commit

---
+++ Transcord/TranscordApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscordApp.swift
+//  Transcord
+//
+//  Created by jinwoong Kim on 2023/10/05.
+//
+
+import SwiftUI
+
+@main
+struct TranscordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
