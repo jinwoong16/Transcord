d59002bf444b69f90f5f78980450869acbc33b35
BoseongGeum <rmaqhtjd8364@naver.com>
2023-11-17 15:59:24 +0900

    feat: recordView

---
+++ RecordView/RoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  RoadingView.swift
+//  Viewer
+//
+//  Created by 금보성 on 11/9/23.
+//
+
+import SwiftUI
+
+struct RoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoadingView()
+}
