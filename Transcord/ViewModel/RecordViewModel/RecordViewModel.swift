88f1f26ae27de9956216aa6dcc2f939be052b959
jinwoong <baseball0020@gmail.com>
2023-11-19 19:20:05 +0900

    feat: create empty view model for RecordView

---
+++ Transcord/ViewModel/RecordViewModel/RecordViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  RecordViewModel.swift
+//  Transcord
+//
+//  Created by jinwoong Kim on 11/19/23.
+//
+
+import Foundation
+import PWRecordKit
+
+final class RecordViewModel: ObservableObject {
+    private let audioRecorder: AudioRecorder
+    
+    @Published private(set) var isRecording: Bool = false
+    
+    
+    init(audioRecorder: AudioRecorder) {
+        self.audioRecorder = audioRecorder
+    }
+    
+    func startRecord() {
+        Task {
+            await MainActor.run {
+                isRecording = true
+            }
+        }
+    }
+    
+    func stopRecord() {
+        Task {
+            await MainActor.run {
+                isRecording = false
+            }
+        }
+    }
+}
