3489c9ca29768e8686279bb9094966e61215fd54
jinwoong <baseball0020@gmail.com>
2023-11-19 18:38:13 +0900

    feat: define an audio object

---
+++ Transcord/Model/Audio.swift
@@ -0,0 +1,14 @@
+//
+//  Audio.swift
+//  Transcord
+//
+//  Created by jinwoong Kim on 11/18/23.
+//
+
+import Foundation
+
+struct Audio: Identifiable {
+    let id: UUID
+    let title: String
+    let url: URL
+}
