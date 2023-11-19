e27b96c35718e845c50124bfa1589e2d3a56d311
jinwoong <baseball0020@gmail.com>
2023-11-19 19:20:36 +0900

    feat: register the dependency as recordViewModel

--- Transcord/TranscordApp.swift
+++ Transcord/TranscordApp.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import PWRecordKit
 
 @main
 struct TranscordApp: App {
+    private let recordViewModel = RecordViewModel(audioRecorder: DefaultAudioRecorder())
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(recordViewModel)
         }
     }
 }
