3b1c1679cf2246857c4e0927392ef759a5c59f52
jinwoong <baseball0020@gmail.com>
2023-12-07 14:38:16 +0900

    feat: add transcript detail view and its list view

--- Transcord/View/TranscribeView/TranscriptListView.swift
+++ Transcord/View/TranscribeView/TranscriptListView.swift
@@ -11,7 +11,22 @@ struct TranscriptListView: View {
     @EnvironmentObject var viewModel: TranscriptListViewModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                ForEach(viewModel.transcripts) { transcript in
+                    NavigationLink(value: transcript) {
+                        TranscriptListRow(transcript: transcript)
+                    }
+                }
+            }
+            .navigationDestination(for: Transcript.self) { transcript in
+                TranscriptDetailView(
+                    viewModel: TranscriptDetailViewModel(
+                        transcript: transcript
+                    )
+                )
+            }
+        }
     }
 }
 
--- Transcord/ViewModel/TranscribeViewModel/TranscriptListViewModel.swift
+++ Transcord/ViewModel/TranscribeViewModel/TranscriptListViewModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Transcript: Identifiable {
+struct Transcript: Identifiable, Hashable {
     let id: UUID
     let title: String
     let url: URL
